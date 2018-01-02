3a69a9860fbe20852fdf0f58bd5a5882002fd211
Toshio Ito <debug.ito@gmail.com>
2018-01-02 09:44:30 +0900

    remove example mapping.

--- settings.js
+++ settings.js
@@ -1,13 +1,3 @@
-// an example to create a new mapping `ctrl-y`
-mapkey('<Ctrl-y>', 'Show me the money', function() {
-    Front.showPopup('a well-known phrase uttered by characters in the 1996 film Jerry Maguire (Escape to close).');
-});
-
-// an example to replace `T` with `gt`, click `Default mappings` to see how `T` works.
-map('gt', 'T');
-
-// an example to remove mapkey `Ctrl-i`
-unmap('<Ctrl-i>');
 
 settings.smoothScroll = false;
 
