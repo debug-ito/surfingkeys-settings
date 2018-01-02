a496a4635e1e6c8601d2eb5da0483e7e9e7cfb6b
Toshio Ito <debug.ito@gmail.com>
2018-01-02 09:18:34 +0900

    initial commit of default settings.

---
+++ settings.js
@@ -0,0 +1,39 @@
+// an example to create a new mapping `ctrl-y`
+mapkey('<Ctrl-y>', 'Show me the money', function() {
+    Front.showPopup('a well-known phrase uttered by characters in the 1996 film Jerry Maguire (Escape to close).');
+});
+
+// an example to replace `T` with `gt`, click `Default mappings` to see how `T` works.
+map('gt', 'T');
+
+// an example to remove mapkey `Ctrl-i`
+unmap('<Ctrl-i>');
+
+// set theme
+settings.theme = `
+.sk_theme {
+    background: #000;
+    color: #fff;
+}
+.sk_theme tbody {
+    color: #fff;
+}
+.sk_theme input {
+    color: #d9dce0;
+}
+.sk_theme .url {
+    color: #2173c5;
+}
+.sk_theme .annotation {
+    color: #38f;
+}
+.sk_theme .omnibar_highlight {
+    color: #fbd60a;
+}
+.sk_theme ul>li:nth-child(odd) {
+    background: #1e211d;
+}
+.sk_theme ul>li.focused {
+    background: #4ec10d;
+}`;
+// click `Save` button to make above settings to take effect.
