9b52e17be3da0daa0a4b859c25592c1761d623fb
Toshio Ito <debug.ito@gmail.com>
2018-01-23 21:32:01 +0900

    add settings about searching.

--- settings.js
+++ settings.js
@@ -53,6 +53,15 @@ vmap('<Ctrl-l>', 'zz');
 
 settings.smoothScroll = false;
 
+// Search settings
+addSearchAliasX('D', 'duckduckgo HTML', 'https://duckduckgo.com/html/?q=', 's', 'https://duckduckgo.com/ac/?q=', function(response) {
+    var res = JSON.parse(response.text);
+    return res.map(function(r){
+        return r.phrase;
+    });
+});
+settings.defaultSearchEngine = 'D';
+
 // Hints settings
 Hints.characters = "asdfghjkl";
 Hints.scrollKeys = "";
